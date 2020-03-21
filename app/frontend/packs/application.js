b933d043aa7f8255f05d5ed18e47dba9900ac32f
Heidi <yuching268@gmail.com>
2020-03-21 17:01:11 +0800

    install bulma

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -15,4 +15,7 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// stimulus的
 import "controllers"
+// bulma的
+import "styles/frontend"
